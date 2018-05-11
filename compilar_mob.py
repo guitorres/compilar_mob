edb8e7ad4c1249fd6afa5165b593d2e58d42fbea
Guilherme Torres <3984835+guitorres@users.noreply.github.com>
2018-05-11 12:05:36 -0300

    Adicionando parâmetros
    Adicionando parâmetros informar url, delimitar posts por quantidade de verdinhas e tamanho da fonte.

--- compilar_mob.py
+++ compilar_mob.py
@@ -1,14 +1,25 @@
 # coding: iso-8859-1
 from bs4 import BeautifulSoup
 import requests, re, pickle, webbrowser, sys
-url = sys.argv[1]
-topico = requests.get(url)
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-t", "--topico", dest="topico",
+                  help="url do topico a ser compilado")
+parser.add_option("-v", "--verdinhas", default=10,
+                  help="compilar apenas posts com quantidade de verdinhas maior ou igual")
+parser.add_option("-f", "--fonte", default=18,
+                  help="tamanho da fonte")                  
+
+(options, args) = parser.parse_args()
+
+topico = requests.get(options.topico)
 pagina_pattern = '<a href="javascript://" class="popupctrl">Página 1 de (.*?)</a>'.decode('iso-8859-1')
 paginas =int(re.search(pagina_pattern, topico.text.encode('iso-8859-1')).group(1))
 arquivo_saida = open('documento.html', 'w')
-verdinhas_corte = int(sys.argv[2])
+#arquivo_saida.write(" <html> <head> <style> *\{font-size:{fonte}pt\} </style></head>".format(fonte=options.fonte))
 for pagina in range(1, paginas+1):
-    url_pagina = url.replace('.html', '-{}.html'.format(pagina) )
+    url_pagina = options.topico.replace('.html', '-{}.html'.format(pagina) )
     topico = requests.get(url_pagina)
     soup = BeautifulSoup(topico.text, 'html.parser')
     posts = soup.find_all('li', class_="postcontainer")    
@@ -19,7 +30,7 @@ for pagina in range(1, paginas+1):
         verdinhas = re.search(r'mais (.*?)</a>', str(texto_verdinhas))
         if verdinhas != None:
             verdinhas = int(verdinhas.group(1))+3
-            if verdinhas > verdinhas_corte:
+            if verdinhas >= options.verdinhas:
                 autor = soup.find('a', class_="username").find('strong').text.encode('iso-8859-1')            
                 data_post = soup.find('span', class_="date").text.encode('iso-8859-1')
                 cabecalho = '<p><b>Autor:</b> {autor} <b>Verdinhas:</b> {verdinhas} <b>Data:</b> {data_post}</p>'\
@@ -30,4 +41,6 @@ for pagina in range(1, paginas+1):
                 melhores_posts.append(html)
     for post in melhores_posts:            
         arquivo_saida.write("%s\n" % post)
+
+arquivo_saida.write("</html>")        
 webbrowser.open("documento.html")      
