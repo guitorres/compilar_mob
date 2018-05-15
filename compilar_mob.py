4acdfeee55cf528c934d3f531730d739704a2ccf
Guilherme Torres <3984835+guitorres@users.noreply.github.com>
2018-05-15 13:34:49 -0300

    Simplificando e ajustes.

--- compilar_mob.py
+++ compilar_mob.py
@@ -1,4 +1,3 @@
-# coding: iso-8859-1
 from bs4 import BeautifulSoup
 import requests, re, pickle, webbrowser, sys
 from optparse import OptionParser
@@ -9,12 +8,14 @@ parser.add_option("-v", "--verdinhas", default=10)
 parser.add_option("-f", "--fonte", default=18)                  
 (options, args) = parser.parse_args()
 
-arquivo = 'documento.html'
 topico = requests.get(options.topico)
 paginas =int(re.search('1 de (.*?)</a>', topico.text).group(1))
+
+arquivo = 'documento.html'
 arquivo_saida = open(arquivo, 'w')
-arquivo_saida.write('<!DOCTYPE html> <html> <head><meta charset="ISO-8859-1"/> <title>Teste</title> <style> *{{font-size:{fonte}pt}} </style></head>'\
+arquivo_saida.write('<!DOCTYPE html> <html> <head><meta charset="UTF-8"/> <title>Teste</title> <style> *{{font-size:{fonte}pt}} </style></head>'\
     .format(fonte=options.fonte))
+
 for pagina in range(1, paginas+1):
     url_pagina = options.topico.replace('.html', '-{}.html'.format(pagina) )
     topico = requests.get(url_pagina)
@@ -29,15 +30,15 @@ for pagina in range(1, paginas+1):
             verdinhas = int(verdinhas.group(1))+3
             if verdinhas >= int(options.verdinhas):
                 autor = soup.find('a', class_="username").find('strong').text
-                data_post = soup.find('span', class_="date").text
-                cabecalho = '<p><b>Autor:</b> {autor} <b>Verdinhas:</b> {verdinhas} <b>Data:</b> {data_post}</p>'\
-                            .format(autor=autor, verdinhas=verdinhas, data_post=data_post)
+                cabecalho = '<p><b>Autor:</b> {autor} <b>Verdinhas:</b> {verdinhas}</p>'\
+                            .format(autor=autor, verdinhas=verdinhas)
 
                 texto_post = soup.find('div', class_="content")
+                print(texto_post)
                 html = '{cabecalho}<p>{post}</p>'.format(cabecalho=cabecalho, post=texto_post)
                 melhores_posts.append(html)                
     for post in melhores_posts:            
-        arquivo_saida.write("%s\n" % post)
+        arquivo_saida.write("{}\n".format(post))
 
 arquivo_saida.write("</html>")        
 webbrowser.open(arquivo)      
