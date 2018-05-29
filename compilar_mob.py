f33430e973312f6e9b7f621f4be7247b5b2c2c37
Guilherme Torres <3984835+guitorres@users.noreply.github.com>
2018-05-29 12:23:16 -0300

    Parâmetro para página inicial (-b) e página final (-e)

--- compilar_mob.py
+++ compilar_mob.py
@@ -6,16 +6,21 @@ parser = OptionParser()
 parser.add_option("-t", "--topico")
 parser.add_option("-v", "--verdinhas", default=10)
 parser.add_option("-f", "--fonte", default=18)                  
+parser.add_option("-b", "--pagina_inicial", default=1)
+parser.add_option("-e", "--pagina_final", default=0)
 (options, args) = parser.parse_args()
 
 topico = requests.get(options.topico)
-paginas =int(re.search('1 de (.*?)</a>', topico.text).group(1))
+paginas = int(options.pagina_final)
+if paginas == 0:
+    paginas =int(re.search('1 de (.*?)</a>', topico.text).group(1))
+
 
 arquivo = 'saida.html'
 arquivo_saida = open(arquivo, 'w')
 arquivo_saida.write('<!DOCTYPE html> <html> <head><meta charset="UTF-8"/> <title>Teste</title> <style> *{{font-size:{fonte}pt}} </style></head>'.format(fonte=options.fonte))
 
-for pagina in range(1, paginas+1):
+for pagina in range(int(options.pagina_inicial), paginas+1):
     url_pagina = options.topico.replace('.html', '-{}.html'.format(pagina) )
     posts = BeautifulSoup(requests.get(url_pagina).text, 'html.parser').find_all('li', class_="postcontainer")
     melhores_posts = []
@@ -25,7 +30,7 @@ for pagina in range(1, paginas+1):
         verdinhas = re.search(r'mais (.*?)</a>', str(texto_verdinhas))        
 
         if (texto_verdinhas == None):
-            verdinhas = 0
+                verdinhas = 0
         elif (verdinhas == None):
             verdinhas = texto_verdinhas.find_all('a')
             verdinhas = len(verdinhas)
@@ -35,8 +40,7 @@ for pagina in range(1, paginas+1):
         autor = soup.find('a', class_="username")
         if (verdinhas >= int(options.verdinhas)) and (autor != None):            
             autor = autor.find('strong').text
-            cabecalho = '<p><b>Autor:</b> {autor} <b>Verdinhas:</b> {verdinhas}</p>'\
-                        .format(autor=autor, verdinhas=verdinhas)
+            cabecalho = '<p><b>Autor:</b> {autor} <b>Verdinhas:</b> {verdinhas}</p>'.format(autor=autor, verdinhas=verdinhas)
 
             paragrafo_post = '<p>{}</p>'.format(soup.find('div', class_="content"))
             html = '<hr> {cabecalho} {post}'.format(cabecalho=cabecalho, post=paragrafo_post)
